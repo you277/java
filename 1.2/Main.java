08cc0a93636c2cb7e4fc9c55ec2c84c54a87b877
you <144391848+you277@users.noreply.github.com>
2023-09-11 15:09:30 -0400

    t

---
+++ 1.2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        final int life = 42; // meaning of life!! dont change!!
+        double hi = 2147483647;
+        boolean joe = false;
+        System.out.println(life);
+        System.out.println(hi);
+        System.out.println(joe);
+    }
+}
