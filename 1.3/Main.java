bb0357ee2e90aba8ea0e730d06408d4b8760e031
you <144391848+you277@users.noreply.github.com>
2023-09-13 15:11:46 -0400

    add scanner thing

--- 1.3/Main.java
+++ 1.3/Main.java
@@ -1,39 +1,64 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        boolean doThing = true;
+        int i = 1;
+        double d = 2.0;
+        double sum = d / i;
+        int definitelyCorrectValue = 4 / 3;
+        int mult = definitelyCorrectValue * 3;
+        double actuallyCorrectValue = 4.0 / 3;
 
+        mult += 2;
+        mult /= 3;
+        mult *= 4;
+        mult ^= 8;
+
+        System.out.println(sum);
+
+        System.out.println("things:");
+        System.out.println(definitelyCorrectValue);
+        System.out.println(actuallyCorrectValue);
+
+        int sumThing = i + definitelyCorrectValue;
+
+        System.out.println("sumThing:");
+        System.out.println(sumThing);
+        System.out.println("other thing:");
+        System.out.println(mult + actuallyCorrectValue / definitelyCorrectValue % d);
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("do u want to do the thing (y/n): ");
+        String doThing = s.nextLine();
+
+        if (!doThing.equals("y") && !doThing.equals("n")) {
+            System.out.println("NOT VALID INPUT!!!");
+            s.close();
+            return;
+        }
+
+        if (doThing.equals("n")) {
+            System.out.println("ok");
+            s.close();
+            return;
+        }
         try {
-            int i = 1;
-            double d = 2.0;
-            double sum = d/i;
-            int definitelyCorrectValue = 4/3;
-            int mult = definitelyCorrectValue * 3;
-            double actuallyCorrectValue = 4.0/3;
+            System.out.println("give me an int and im gonna divide mult (" + mult + ") by it: ");
+            String div = s.nextLine();
+            s.close();
 
-            mult += 2;
-            mult /= 3;
-            mult *= 4;
-            mult ^= 8;
-
-            System.out.println(sum);
-
-            System.out.println("things:");
-            System.out.println(definitelyCorrectValue);
-            System.out.println(actuallyCorrectValue);
-
-            int sumThing = i + definitelyCorrectValue;
-
-            System.out.println("sumThing:");
-            System.out.println(sumThing);
-            System.out.println("other thing:");
-            System.out.println(mult + actuallyCorrectValue / definitelyCorrectValue % d);
-
-            if (doThing) {
-                System.out.println(0 / 0);
-                System.out.println("what da");
+            int divisor;
+            try {
+                divisor = Integer.parseInt(div);
+            } catch (Exception e) {
+                System.out.println("NOT AN INT!!!!!!");
+                return;
             }
+
+            System.out.println(mult / divisor);
+            System.out.println("what da congerartion :tada:");
         } catch (Exception e) {
-            System.out.println("cool error");
+            System.out.println("it errored bruh why would you input 0");
             System.out.println(e);
         }
     }
